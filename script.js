dddec580b93ab2dba9bf1fe48d35589c2ac50481
Jakehu2020 <71296004+Jakehu2020@users.noreply.github.com>
2024-07-24 10:06:06 -0500

    Step 1

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.querySelector(".down").addEventListener("click",() => {
     document.querySelector(".down").style.opacity = "0";
     document.querySelector(".down").style.cursor = "auto";
     
-    scrollTo(0, 500);
+    scrollTo(0, 1000);
 })
