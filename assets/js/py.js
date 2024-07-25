529a83977e3f5a4a590baa591861cd12c6a5d208
Jakehu2020 <71296004+Jakehu2020@users.noreply.github.com>
2024-07-25 11:05:59 -0500

    Save

---
+++ assets/js/py.js
@@ -0,0 +1,43 @@
+let webout = document.querySelector(".webout");
+const editor = document.querySelector(".code");
+
+let shadow = webout.attachShadow({ mode: "open" });
+let content = document.createElement("div");
+shadow.appendChild(content);
+function run() {
+//   
+}
+function stop() {
+  
+}
+//
+(async () => {
+  console.log("1");
+  content.innerText = 'Initializing...\n'
+
+  window.pyodide = await loadPyodide({stdout: addToOutput, stderr: addToOutput})
+  
+        content.innerText += 'Ready!\n' 
+})()
+
+
+function addToOutput(s) {
+  content.innerText += `${s}\n`
+  content.scrollTop = content.scrollHeight
+}
+
+async function evaluatePython(x) {
+  await pyodide.loadPackagesFromImports(x, addToOutput, addToOutput)
+  try {
+    let result = await pyodide.runPythonAsync(x)
+    addToOutput(`${result}`)
+  }
+  catch (e) {
+    addToOutput(`${e}`)
+  }
+  code.value = ''
+}
+// 
+document.querySelector(".run", (e) => {
+ content.innerHTML+="<hr>"; evaluatePython(editor.innerText);
+})
